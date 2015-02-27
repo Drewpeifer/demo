e8d525b706eaac13d05d4ae6bbb9ace897901fef
Drew Peifer <peiftronica@gmail.com>
2015-02-27 02:34:38 -0500

    WIP building in arrival events, debugged out fuel messages

--- dopespace/js/travel.js
+++ dopespace/js/travel.js
@@ -1,3 +1,27 @@
+function randomIncident() {
+    // incident props:
+    // title, description, alternatives, choices, outcomes, rewards, hasHappened
+
+
+    // pick a random incident from incidents.js
+    incident = incidents[Math.floor(Math.random()*incidents.length)];
+    alternative = incident.alternatives[Math.floor(Math.random()*incident.alternatives.length)];
+    choices = incident.choices;
+    console.dir(incident);
+
+
+    incidentDescrip = incident.description + " " + alternative;
+
+
+    showAlert(incident.title, incidentDescrip);
+    $.each(choices, function(index, value) {
+        $('.alert').append(
+                '<button class="alert-action" data-index="' + index + '" >' +
+                value + '</button>'
+            );
+    });
+}
+
 // travelling between locations
 function travel() {
     fuelTank = $('.fuel p'),// fuel label container
@@ -11,7 +35,8 @@ function travel() {
     });
 
     if (fuel == 0) {
-        showAlert('Out of fuel!', 'You\'re stuck, unless your current port has fuel to sell. And you have some money, right?');
+        // TODO: more subtle fuel alerts
+        //showAlert('Out of fuel!', 'You\'re stuck, unless your current port has fuel to sell. And you have some money, right?');
 
     } else if (portTitle.text() === nextPort) {
         showAlert('You\'re already at that location', 'What\'re you, some kind of jokester?');
@@ -26,6 +51,11 @@ function travel() {
             fuelTank.removeClass('invalid').addClass('valid');
         }
 
+        if (newFuel == 1) {// warn pilot if 1 gallon of fuel left
+            // TODO: more subtle fuel alerts
+            //showAlert('You\'re almost out of fuel!', 'Hope you can reach a fuel station, genius.');
+        } else {}
+
         portTitle.text(port[0].title);// set new currentPort
         portDescription.text(port[0].description);// set new descrip
         stockMarket();// build/rebuild
@@ -35,9 +65,7 @@ function travel() {
                                .click(buyFuel);// but i like semantics
         $('.map').slideUp();// hide map
 
-        if (newFuel == 1) {// warn pilot if 1 gallon of fuel left
-            showAlert('You\'re almost out of fuel!', 'Hope you can reach a fuel station, genius.');
-        } else {}
+        randomIncident();// arrival event occurs (or not)
 
         evalLootStockCargo();// check new location's stock levels
     }
