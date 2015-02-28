4445d423d3da442f3e51bf66afb607e6c81cad40
Drew Peifer <peiftronica@gmail.com>
2015-02-28 04:35:15 -0500

    built QAD sales and hikes into travel()

--- dopespace/js/market.js
+++ dopespace/js/market.js
@@ -50,6 +50,48 @@ function stockMarket() {
             '</tr>');
     });
 
+    marketFlux = getRandomNumber(1,3);// returns 1 or 2, max is actually exclusive
+    console.log('marketFlux = ' + marketFlux);
+    if (marketFlux == 1) {
+        // no sale or hike today
+        console.log('No sale/hike today!');
+    } else {
+        // sale/hike today!
+        saleOrHike = getRandomNumber(1,3),// returns 1 or 2
+        flux = getRandomNumber(2,6),// returns 2 thru 5
+        randomStock = menu[Math.floor(Math.random()*menu.length)];// pick random stock
+        console.log('saleOrHike = ' + saleOrHike);
+        console.log('flux = ' + flux);
+        console.log('randomStock = ' + randomStock.title + ' @ ' + randomStock.basePrice);
+
+
+        if (saleOrHike == 1) {
+            // it's a sale!
+            // find randomLoots price cell
+            targetPriceCell = $('.market td:contains("' + randomStock.title + '")').siblings('.price');
+            targetPrice = targetPriceCell.children('p');
+            targetPriceVal = targetPrice.children('p').text();
+            // recalc value
+            newPriceVal = Math.floor(parseInt(randomStock.basePrice) / flux);
+            //set new loot value in DOM
+            targetPrice.text(newPriceVal);
+            targetPrice.addClass('sale');// tag as sale price
+            console.log('Sale! Today, ' + randomStock.title + " only costs " +  newPriceVal);
+        } else {
+            // it's a hike!
+            // find randomLoots price cell
+            targetPriceCell = $('.market td:contains("' + randomStock.title + '")').siblings('.price');
+            targetPrice = targetPriceCell.children('p');
+            targetPriceVal = targetPrice.children('p').text();
+            // recalc value
+            newPriceVal = Math.floor(parseInt(randomStock.basePrice) * flux);
+            //set new loot value in DOM
+            targetPrice.text(newPriceVal);
+            targetPrice.addClass('hike');// tag as hike price
+            console.log('Hike! Today, ' + randomStock.title + " costs " +  newPriceVal);
+        }
+    }
+
 }
 
 // check each stock and loot amount,
