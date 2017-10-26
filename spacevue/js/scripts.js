489a3b8d13ab8f783c8eb9600af2b7dee5817899
Drew Peifer <peiftronica@gmail.com>
2017-10-26 12:58:15 -0400

    now  building fuel and cargo rows in market

--- spacevue/js/scripts.js
+++ spacevue/js/scripts.js
@@ -8,6 +8,8 @@ var app = new Vue({
 		data: stats
 });
 
+// Market stuff
+
 /**
  * Returns a random integer between min (inclusive) and max (inclusive)
  * Using Math.round() will give you a non-uniform distribution!
@@ -23,7 +25,29 @@ function buildMarket() {
 
 	// clear out old menu data
 	marketTable.empty();
-
+	// add column headers
+    marketTable.append('<li class="market-header"><span class="title">Commodity' +
+				' (Sale/Hike)</span>' +
+				'<span class="action">SLIDER + ACTION</span>' +
+				'<span class="num">Price</span>' +
+				'<span class="num">Stock</span>' +
+				'<span class="num">Loot</span></li>');
+    // check for fuel / upgrade availability, build rows if true
+    if (currentPort.fuelStation) {
+marketTable.append('<li><span class="title">Fuel</span>' +
+					'<span class="action">SLIDER + ACTION</span>' +
+					'<span class="num">' + currentPort.fuelPrice + '</span>' +
+					'<span class="num">' + currentPort.fuelAvailable + '</span>' +
+					'<span class="num">' + stats.fuel + '</span></li>');
+    } else if (port.cargoUpgrade) {
+		marketTable.append('<li><span class="title">Cargo Hold Upgrade</span>' +
+			'<span class="action">SLIDER + ACTION</span>' +
+			'<span class="num">' + currentPort.cargoUpgradePrice + '</span>' +
+			'<span class="num">' + stats.remainingCargoUpgrades + '</span>' +
+			'<span class="num">' + (config.cargoUpgrades - stats.remainingCargoUpgrades) + '</span></li>');
+    } else {
+		// do nothing
+	}
 	// populate the marketTable
 	menuA.forEach( function (stock) {
 
@@ -34,12 +58,12 @@ function buildMarket() {
         minPrice = stock.basePrice + (stock.basePrice * -currentPort.priceFlux),
         maxPrice = stock.basePrice + (stock.basePrice * currentPort.priceFlux),
         newPrice = getRandomStock(minPrice, maxPrice);// set random price value within min/max range
-        // append row to UI
-        marketTable.append('<li><span>' + stock.title + '</span>' +
-					'<span>Sale/Hike</span>' +
-					'<span>SLIDER + ACTION</span>' +
-					'<span>' + newPrice + '</span>' +
-					'<span>' + newStock + '</span>' +
-					'<span>' + stock.loot + '</span></li>');
+        // append commodity row to UI
+        marketTable.append('<li><span class="title">' + stock.title +
+					' (Sale!)</span>' +
+					'<span class="action">SLIDER + ACTION</span>' +
+					'<span class="num">' + newPrice + '</span>' +
+					'<span class="num">' + newStock + '</span>' +
+					'<span class="num">' + stock.loot + '</span></li>');
     });
 }
