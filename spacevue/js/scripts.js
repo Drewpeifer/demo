68d60fac257cd42c2c2cc9e9426b902a7b29b15f
Drew Peifer <peiftronica@gmail.com>
2017-10-27 15:09:10 -0400

    now showing zero state for buy/sell buttons

--- spacevue/js/scripts.js
+++ spacevue/js/scripts.js
@@ -30,12 +30,20 @@ Vue.component('marketplace', {
                     '<span class="action">' +
                     '<input type="range" v-model="item.delta" value="0" v-bind:max="item.currentStock" v-bind:min="0 - item.currentLoot">' +
                     '</span>' +
-                    '<span class="exchange"><button>{{ (item.delta > 0) ? "Buy" : "Sell" }}</button></span>' +
+                    '<span class="exchange"><button>{{ exchangeText(item.delta) }}</button></span>' +
                     '<span class="num">{{ item.currentPrice }}</span>' +
                     '<span class="num">{{ item.currentStock }}</span>' +
                     '<span class="num">{{ item.currentLoot }}</span>' +
                 '</li>' +
             '</ul>',
+    methods: {
+       exchangeText(item){
+        console.dir(item);
+        if (item > 0) return "Buy"
+        if (item < 0) return "Sell"
+        return "..."
+      }
+    },
     data() {
         return {
             mainStats: stats
