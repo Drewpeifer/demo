122fa279948af0e89c3a4f1f113dc88c6cbf1926
Drew Peifer <peiftronica@gmail.com>
2017-11-08 12:34:59 -0500

    built incident[3]

--- spacevue/js/incidents-2.js
+++ spacevue/js/incidents-2.js
@@ -154,6 +154,47 @@ incidents[2] = {
 	hasHappened : false
 }
 
+incidents[3] = {
+	type : "Distress Signal",
+	description : "Captain, we've received a distress signal from an unknown vessel. " +
+				  "They say their engines are malfunctioning, and exhaust plasma has leaked into the ship's engineering section. " +
+                  "Scanners show their engines are indeed operating at 10 percent efficiency, and their life support systems are overloading " +
+                  "one by one, radiating out from the engineering section. Their weapons are offline and their shields are down, they " +
+                  "may be diverting power to life support but the damage may have affected those systems as well.",
+	alternatives : [
+        "We may be the only ship within range that can help, and judging by their ship registration, they are a private transport ship for a local planetary government. Too small to carry anyone important, probably just shuttling bureaucrats from system to system.",
+        "That radiation may affect our systems if we attempt to dock or approach too close. Their engines may also overload at any time, and the resulting explosion would severly damage our ship if we were within docking range. I recommend assisting in any way we can, from a healthy distance. If necessary, we can evacuate their personnel.",
+        "Keep in mind, some pirates use this same type of scenario to lure in repair vessels, then rob them blind and sometimes even kill the crew. Something seems fishy to me, Captain, but I did have fish for lunch, so there's that."
+	],
+	choices : [
+        "Tractor beam them as close as possible without endangering our ship. Beam aboard an away team in hazard suits to investigate the engine room trouble. If we can fix it before we all explode, perfect. If not, beam their entire crew aboard and we warp out of here like we stole something.",
+        "Maintain a safe distance. Try to assist remotely, either by advising over comms or transporting supplies aboard. We can't afford to get close enough to dock or evacuate.",
+        "Something's fishy here. Let's wait a few minutes and continue scanning their systems. If they're not lying, their engine core should overheat shortly, and we can move in and evacuate. If they are lying or turn hostile, detonate their core with a phaser blast."
+	],
+	outcomes : {
+		0 : {
+			type : "good",
+			description : "Ridiculous. They had their manifolds set to minimum and their ionization compensator set to 80 percent, what a bunch of dummies! Our engineers pushed a few buttons and sent them on their way. They paid us well for our time though, probably out of embarassment.",
+			effect : "+1000 Credits",
+			func : function() { return walletChange(1000); }
+		},
+		1 : {
+			type : "good",
+			description : "We were able to identify the trouble and Engineering beamed over some parts, but when they powered up their teleporter to receive the parts, their electrical systems shorted out. Life support started failing so we beamed the crew of twelve aboard immediately, and their ship exploded shortly thereafter. They didn't seem too upset, apparently it was a rental, and they plan to leave at the next port. At least we salvaged one unit of fuel from their wreckage.",
+			effect : "+1 fuel",
+			func : function() { return fuelChange(1); }
+		},
+		2 : {
+			type : "neutral",
+			description : "Sensors indicate they're low on fuel and oxygen. There's minor radiation all over the ship but levels increase closer to the engineering section, peaking at levels of WHOA they just exploded. I think we waited too long to help, Captain. We should probably just move along now...",
+			effect : "No consequence",
+			func : function() { return nothingHappened(); }
+		},
+	},
+	isHappening : false,
+	hasHappened : false
+}
+
 // incidents[X] = {
 // 	type :
 // 	description :
