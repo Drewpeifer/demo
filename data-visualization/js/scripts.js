2b0d9b4218c3ec1bfbb7f08fc37ed5bb737e4ff6
Drew Peifer <peiftronica@gmail.com>
2019-05-12 03:26:52 -0400

    stubbed out some d3 examples

--- data-visualization/js/scripts.js
+++ data-visualization/js/scripts.js
@@ -1,4 +1,34 @@
+// add commas to long numbers
+$.fn.digits = function(){ 
+    return this.each(function(){ 
+        $(this).text( $(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") ); 
+    })
+}
+
+function populateD3(data) {
+	var populations = data;
+
+	d3.select("#tab-1")
+		.selectAll("p")
+		.data(populations)
+		.enter()
+		.append("p")
+			.style("width", function(d) { newWidth = d.value/500000 + "px"; return newWidth; })
+			.style("background-image","linear-gradient(to left, orange, red)")
+			.text(function(d) {
+				newValue = d.value.toLocaleString('en');
+				return "("+d.date+") - "+newValue });
+}
+
 $(function() {
+	// initialize d3.js demo
+	$.getJSON('data/us-population-1960-2019.json', function(data) {
+		var populations = data;
+		populateD3(populations);
+	});
+	
+	$('#tab-1 p').digits();
+
 	// jqueryUI, etc.
 	$(".content").tabs();
 	$(".widget input[type=submit], .widget a, .widget button").button();
