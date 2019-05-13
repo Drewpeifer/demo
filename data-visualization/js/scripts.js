3508ac2de00ee8b43834f22ecde0064732b34b97
Drew Peifer <peiftronica@gmail.com>
2019-05-13 05:52:08 -0400

    finished round 1 of Nobel app

--- data-visualization/js/scripts.js
+++ data-visualization/js/scripts.js
@@ -1,5 +1,6 @@
 // populate the D3 population demo
-function buildPopulations(populations) {
+function buildPopulationApp(populations) {
+	console.log('building populationApp')
 	maxPop = parseInt(populations[0].value);
 
 	d3.select("#populations")
@@ -26,6 +27,7 @@ function buildPopulations(populations) {
 
 // build planetary gravity Vue demo
 function buildPlanetApp(planetData) {
+	console.log('building planetApp');
 	var app = new Vue({
 		el: '#planetApp',
 		data: {
@@ -34,7 +36,6 @@ function buildPlanetApp(planetData) {
 			},
 		methods: {
 			scaleRatio : function(planet) {
-				console.log('scaleRatio fired', planet);
 				return (1/planet.relative_equatorial_diameter);
 			},
 			axialRotation : function(planet) {
@@ -54,15 +55,84 @@ function buildPlanetApp(planetData) {
 	});
 }
 
+// build nobel laureate Chart.js demo
+function buildNobelApp(nobel) {
+	console.log('building nobelApp');
+	var labels = nobel.laureates.map(function(e) {
+	   return e.bornCountry;
+	});
+	var data = [];
+
+	function countLabels(labels) {
+	    var a = [], b = [], prev;
+	    
+	    labels.sort();
+	    for ( var i = 0; i < labels.length; i++ ) {
+	        if ( labels[i] !== prev ) {
+	            a.push(labels[i]);
+	            b.push(1);
+	        } else {
+	            b[b.length-1]++;
+	        }
+	        prev = labels[i];
+	    }
+	    
+	    return [a, b];
+	}
+
+	var chartData = countLabels(labels);
+	// chartData[0] = labels, [1] = data
+
+    colorArray = [];
+    for (var z = 0; z < chartData[0].length; z++) {
+    	var letters = '0123456789ABCDEF'.split('');
+    	var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * 16)];
+	    }
+	    colorArray.push(color);
+	}
+	console.log('colorArray ' + colorArray);
+
+	var ctx = document.getElementById('nobelChart').getContext('2d');
+	var myChart = new Chart(ctx, {
+	    type: 'doughnut',
+	    data: {
+	        labels: chartData[0],
+	        datasets: [{
+	            label: '# of Laureates',
+	            data: chartData[1],
+	            backgroundColor: colorArray,
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero: true
+	                }
+	            }]
+	        }
+	    }
+	});
+}
+
 $(function() {
 	// initialize populations / D3.js demo
 	$.getJSON('data/us-population-1960-2019.json', function(data) {
-		buildPopulations(data);
+		buildPopulationApp(data);
 	});
 
 	//initialize planetary Vue demo
 	$.getJSON('data/celestial-bodies.json', function(data) {
 		buildPlanetApp(data);
+	});
+
+	// initialize nobel / chart.js demo
+	$.getJSON('data/nobel-laureates.json', function(data) {
+		console.log('getting json');
+		buildNobelApp(data);
 	});
 
 	// jqueryUI, etc.
